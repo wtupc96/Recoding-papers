b55ce3f3eacc55b32c7ef65836245088759d6031
Peter Chan <peter_ouyang@outlook.com>
2019-06-09 21:49:05 +0800

    Finished code and md.

--- 1_Distilling_the_knowledge_in_a_Neural_Network/Distilling the Knowledge in a Neural Network.md
+++ 1_Distilling_the_knowledge_in_a_Neural_Network/Distilling the Knowledge in a Neural Network.md
@@ -6,9 +6,17 @@
 - **Main idea:**
   - Extract the distribution of training data from one *COMPLEX* model(Teacher Net) and "teach" the distribution to the *EASY* model(Student Net). The processed is called "distill".
   - <u>To raise the temperature of the final softmax until the cumbersome model produces a suitably soft set of targets to be used for training the small model to match these soft target.</u>
+  - e.g.
+    - For the Teacher Network:
+      - The probability of 3 is 0.998 which is ground-truth, and the probability of 5 and 7 are 0.004 and 0.005 respectively. 
+      - $P('3') >> P('5')$ and $P('3') >> P('7')$
+      - But $P('5')\approx P('7')$, which means Number '3' is somewhat like Number '2' or Number '7', this is the knowledge, image-label could not tell us but can be learned using an model(Teacher Network), is called 'dark knowledge'.
+      - And now we want to 'teach' this 'dark knowledge' to another model(Student Network).
+    - For the Student Network:
+      - We wanna magnify this knowledge by 'distilling' it(use the formula below).
+      - And learn this magnified 'dark knowledge' by making both this two networks output the same 'dark knowledge' with the same input.
 
 - **Core Concept:**
-
   - $q_i=\frac{e^{z_i/T}}{\sum_je^{z_j/T}}=\frac{1}{\sum_je^{[(z_j-z_i)/T]}}$
     - Saying that $q_i$ is the "softmax" output layer that converts the logits, and $T$ is the temperature.
     - When $T=1$, $q_i$ is the "classical" calculation of "softmax".
--- 1_Distilling_the_knowledge_in_a_Neural_Network/teach_nets_soft_targets.py
+++ 1_Distilling_the_knowledge_in_a_Neural_Network/teach_nets_soft_targets.py
@@ -1,4 +1,5 @@
 import sys
+import time
 
 sys.path.append(r'F:\recoding_papers')
 
@@ -12,7 +13,7 @@ from glob_params import MNIST_DATASET
 mnist = read_data_sets(train_dir=MNIST_DATASET, one_hot=True)
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--temperature', '-T', type=float, required=True,
+parser.add_argument('--temperature', '-T', type=float, default=-1,
                     help='The temperature for DISTILLING.')
 parser.add_argument('--batch_size', '-B', type=int, default=1000,
                     help='The batch size of taking images in.')
@@ -29,18 +30,18 @@ logger = logging.getLogger(__name__)
 
 
 def net(inputs, net_name: str):
-    if net_name == 'teacher':
-        num_output = 400
+    if 'teacher' in net_name:
+        num_output = 50
         norm_fn = slim.batch_norm
         do_dropout = False
-    elif net_name == 'student':
-        num_output = 200
+    elif 'student' in net_name:
+        num_output = 30
         norm_fn = slim.batch_norm
         do_dropout = False
     else:
         raise Exception('Unrecognized NET.')
 
-    with tf.variable_scope(name_or_scope=net_name):
+    with tf.variable_scope(name_or_scope=net_name, reuse=False):
         with slim.arg_scope([slim.fully_connected],
                             num_outputs=num_output, normalizer_fn=norm_fn):
             fc1 = slim.fully_connected(inputs=inputs, scope='fc1')
@@ -55,42 +56,46 @@ def net(inputs, net_name: str):
         return output
 
 
-def teacher_net(inputs):
-    return net(inputs=inputs, net_name='teacher')
+def teacher_net(inputs, identifier: str):
+    return net(inputs=inputs, net_name='teacher' + identifier)
 
 
-def student_net(inputs):
-    return net(inputs=inputs, net_name='student')
+def student_net(inputs, identifier: str):
+    return net(inputs=inputs, net_name='student' + identifier)
 
 
-def train():
+def train(temperature: float,
+          batch_size: int,
+          epoch_teacher: int,
+          epoch_student_individual: int,
+          epoch_student_learning: int):
+    tf.set_random_seed(1)
+
     X = tf.placeholder(dtype=tf.float32, shape=[None, 784], name='X')
     Y = tf.placeholder(dtype=tf.float32, shape=[None, 10], name='Y')
 
-    output_teacher = teacher_net(X)
-    output_student = student_net(X)
+    identifier = str(time.time())
+    output_teacher = teacher_net(X, identifier)
+    output_student = student_net(X, identifier)
 
     loss_teacher = slim.losses.softmax_cross_entropy(logits=output_teacher, onehot_labels=Y, scope='loss_teacher')
     loss_student = slim.losses.softmax_cross_entropy(logits=output_student, onehot_labels=Y, scope='loss_student')
 
-    temperature = args.temperature
     loss_student_learning = temperature ** 2 * slim.losses.mean_squared_error(
         predictions=slim.softmax(output_student / temperature),
         labels=slim.softmax(output_teacher / temperature))
 
     vars = tf.trainable_variables()
 
-    vars_teacher = [v for v in vars if 'teacher' in v.name]
-    vars_student = [v for v in vars if 'student' in v.name]
+    vars_teacher = [v for v in vars if 'teacher' + identifier in v.name]
+    vars_student = [v for v in vars if 'student' + identifier in v.name]
 
-    logger.info(vars_teacher)
-    logger.info(vars_student)
-
-    optimizer = tf.train.AdamOptimizer(learning_rate=0.005)
+    optimizer = tf.train.AdamOptimizer(learning_rate=0.1)
+    sgd_optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.1)
 
     opt_teacher = optimizer.minimize(loss_teacher, var_list=vars_teacher, name='loss_teacher')
-    opt_student = optimizer.minimize(loss_student, var_list=vars_student, name='loss_student')
-    opt_student_learning = optimizer.minimize(loss_student_learning, name='loss_student')
+    opt_student = sgd_optimizer.minimize(loss_student, var_list=vars_student, name='loss_student')
+    opt_student_learning = sgd_optimizer.minimize(loss_student_learning, name='loss_student')
 
     correct_prediction_teacher = tf.equal(tf.argmax(Y, 1), tf.argmax(output_teacher, 1))
     correct_prediction_student = tf.equal(tf.argmax(Y, 1), tf.argmax(output_student, 1))
@@ -103,43 +108,69 @@ def train():
         sess.run(init_vars)
 
         logger.info('Student Net is learning.')
-        for cnt in range(args.epoch_student_individual):
-            train_images, train_labels = mnist.train.next_batch(args.batch_size)
+        for cnt in range(epoch_student_individual):
+            train_images, train_labels = mnist.train.next_batch(batch_size)
             _, cost_student = sess.run([opt_student, loss_student],
                                        feed_dict={X: train_images, Y: train_labels})
-            if cnt % (args.epoch_student_individual // 5 + 1) == 0:
+            if cnt % (epoch_student_individual // 5 + 1) == 0:
                 logger.info('Student loss: {}'.format(cost_student))
         acc_student_individual = sess.run(accuracy_student,
                                           feed_dict={X: mnist.test.images, Y: mnist.test.labels})
         logger.info('Student INDIVIDUAL Accuarcy: {}'.format(acc_student_individual))
 
         logger.info('Teacher Net is learning.')
-        for cnt in range(args.epoch_teacher):
-            train_images, train_labels = mnist.train.next_batch(args.batch_size)
+        for cnt in range(epoch_teacher):
+            train_images, train_labels = mnist.train.next_batch(batch_size)
             _, cost_teacher = sess.run([opt_teacher, loss_teacher],
                                        feed_dict={X: train_images, Y: train_labels})
 
             _, cost_student = sess.run([opt_student, loss_student],
                                        feed_dict={X: train_images, Y: train_labels})
-            if cnt % (args.epoch_teacher // 5 + 1) == 0:
+            if cnt % (epoch_teacher // 5 + 1) == 0:
                 logger.info('Teacher loss: {}'.format(cost_teacher))
         acc_teacher = sess.run(accuracy_teacher,
                                feed_dict={X: mnist.test.images, Y: mnist.test.labels})
         logger.info('Teacher Accuracy: {}'.format(acc_teacher))
 
         logger.info('Teacher is teaching Student.')
-        for cnt in range(args.epoch_student_learning):
-            train_images, _ = mnist.train.next_batch(args.batch_size)
+        for cnt in range(epoch_student_learning):
+            train_images, _ = mnist.train.next_batch(batch_size)
             _, cost_student = sess.run([opt_student_learning, loss_student_learning],
                                        feed_dict={X: train_images})
-            if cnt % (args.epoch_student_learning // 5 + 1) == 0:
+            if cnt % (epoch_student_learning // 5 + 1) == 0:
                 logger.info('Student loss: {}'.format(cost_student))
         acc_student_learning = sess.run(accuracy_student,
                                         feed_dict={X: mnist.test.images, Y: mnist.test.labels})
         logger.info('Student LEARNING Accuarcy: {}'.format(acc_student_learning))
-        logger.info('Accuaracy gained by: {}'.format(acc_student_learning - acc_student_individual))
+        enhancement = acc_student_learning - acc_student_individual
+        logger.info('Accuaracy gained by: {}'.format(enhancement))
+        return enhancement
 
 
 if __name__ == '__main__':
     logger.info(args)
-    train()
+    temperature = args.temperature
+    batch_size = args.batch_size
+    epoch_teacher = args.epoch_teacher
+    epoch_student_individual = args.epoch_student_individual
+    epoch_student_learning = args.epoch_student_learning
+
+    if temperature > 0:
+        logger.info('Calc the enhancement.')
+        train(temperature, batch_size, epoch_teacher, epoch_student_individual, epoch_student_learning)
+    else:
+        import numpy as np
+        from matplotlib import pyplot as plt
+        logger.info('Draw the relationship between T and according enhancement.')
+        x = np.linspace(-10, 100, num=300)
+        y = list()
+        for temperature in x:
+            y.append(train(temperature, batch_size, epoch_teacher, epoch_student_individual, epoch_student_learning))
+
+        plt.xlabel('T')
+        plt.ylabel('Enhancement %')
+        idx = y.index(max(y))
+        plt.title('MAX enhancement={}, at T={}'.format(y[idx], x[idx]))
+        plt.plot(x, np.array(y) * 100)
+        plt.savefig('./result.png')
+        plt.show()
