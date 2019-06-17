50c43583e7337975edc57e0b347db05ca7c8b917
Peter Chan <peter_ouyang@outlook.com>
2019-06-17 16:27:40 +0800

    Modified.

--- Visualizing_and_Understanding_Convolutional_Networks/main.py
+++ Visualizing_and_Understanding_Convolutional_Networks/main.py
@@ -8,31 +8,8 @@ from Visualizing_and_Understanding_Convolutional_Networks.deconv import run
 input_image, image_conv1, image_conv2, image_conv3 = train()
 restruct_conv1, restruct_conv2, restruct_conv3 = run(image_conv1, image_conv2, image_conv3)
 
-# fig = plt.figure()
-# ax1 = fig.add_subplot(1, 4, 1)
-# ax1.plot(input_image)
-#
-# # ax2 = fig.add_subplot(1, 7, 2)
-# # ax2.plot(image_conv1)
-# #
-# # ax3 = fig.add_subplot(1, 7, 3)
-# # ax3.plot(image_conv2)
-# #
-# # ax4 = fig.add_subplot(1, 7, 4)
-# # ax4.plot(image_conv3)
-#
-# ax5 = fig.add_subplot(1, 4, 2)
-# ax5.plot(restruct_conv1)
-#
-# ax6 = fig.add_subplot(1, 4, 3)
-# ax6.plot(restruct_conv2)
-#
-# ax7 = fig.add_subplot(1, 4, 4)
-# ax7.plot(restruct_conv3)
-#
-# fig.savefig('full_figure.png')
-
 import cv2
+
 print(input_image.shape)
 cv2.imwrite('1.png', input_image)
 print(image_conv1.shape)
--- glob_params.py
+++ glob_params.py
@@ -1,5 +1,5 @@
-import os
 import logging
+import os
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
@@ -10,3 +10,4 @@ DATASET_ROOT = os.path.join(ROOT, 'dataset')
 
 MNIST_DATASET = os.path.join(DATASET_ROOT, 'mnist')
 CIFAR10_DATASET = os.path.join(DATASET_ROOT, 'cifar-10-batches-py')
+ENGLISH_HAND_DATASET = os.path.join(DATASET_ROOT, 'EnglishHnd', 'English', 'Hnd')
